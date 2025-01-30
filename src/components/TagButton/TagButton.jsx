346be6c1e492794329c7a171d97e05fba3af523c
Hyunjun Lee <leehyunjune11@naver.com>
2025-01-30 22:40:23 +0900

    tagbutton css 수정

--- src/components/TagButton/TagButton.jsx
+++ src/components/TagButton/TagButton.jsx
@@ -2,7 +2,7 @@ import './TagButton.css'
 
 const TagButton = ({ list = [], children, ...props }) => {
     return (
-        <div className="tag-container" {...props}>
+        <div className="tag_container" {...props}>
             {list.length > 0 ? (
                 list.map((item, index) => <span key={index}>{item}</span>)
             ) : (
