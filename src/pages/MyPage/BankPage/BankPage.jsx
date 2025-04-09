b56b39e620c83eecb48ffc75f683af9bcabccc55
Hyunjun Lee <leehyunjune11@gmail.com>
2025-04-10 00:09:31 +0900

    bankpage 수정중

--- src/pages/MyPage/BankPage/BankPage.jsx
+++ src/pages/MyPage/BankPage/BankPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MainLayout from "../../../components/Layout/MainLayout/MainLayout";
 import { validationPatterns } from "../../../core/constants/validationPatterns";
 import ProjectContentBox from "../ProjectManagementPage/components/ProjectContentBox/ProjectContentBox";
@@ -6,6 +6,8 @@ import "./BankPage.css";
 
 const BankPage = () => {
   const [value, setValue] = useState();
+  const [checkedItems, setCheckedItems] = useState([]);
+  const [cancelStatus, setCancelStatus] = useState();
 
   const handleMoneyChange = (e) => {
     const inputValue = e.target.value.replace(/[^0-9]/g, "");
@@ -27,7 +29,35 @@ const BankPage = () => {
     console.log("출금", value);
   };
 
-  const user = "selleb";
+  const checkedName = checkedItems.map((item) => item.name);
+
+  const handleCheckContent = (key, des, checked, name) => {
+    setCheckedItems((prev) =>
+      checked
+        ? [...prev, { key, name, des }]
+        : prev.filter((item) => item.key !== key)
+    );
+  };
+
+  const handleCancelStatus = (value) => {
+    if (value === "yes") {
+      console.log("유저가 취소에 동의함", checkedName);
+    } else {
+      console.log("유저가 취소에 동의하지 않음", checkedName);
+    }
+    setCancelStatus(value);
+  };
+
+  const handleCancelBtn = () => {
+    console.log("취소를 요청한 건", checkedItems);
+  };
+
+  useEffect(() => {
+    console.log("업데이트된 checkedItems:", checkedItems);
+    console.log("선택된 name 목록:", checkedName);
+  }, [checkedItems]);
+
+  const user = "sellecter";
 
   const bankTest = {
     1: {
@@ -120,10 +150,19 @@ const BankPage = () => {
             <div className="bankPage_content_box">
               {Object.entries(bankTest).map(([key, item]) => (
                 <div className="bankPage_sellecter">
-                  <input type="checkbox" key={item.id} />
+                  <input
+                    type="checkbox"
+                    onChange={(e) =>
+                      handleCheckContent(
+                        key,
+                        item.description,
+                        e.target.checked,
+                        item.name
+                      )
+                    }
+                  />
                   <ProjectContentBox
-                    key={item.id}
-                    id={item.id}
+                    id={key}
                     name={item.name}
                     description={item.description}
                     date={item.date}
@@ -132,6 +171,52 @@ const BankPage = () => {
                   />
                 </div>
               ))}
+            </div>
+            <div className="bankPage_cancel_section">
+              <div className="bankPage_cancel_inner">
+                <div className="cancel_inner_title">거래 취소하기</div>
+                <div>
+                  {checkedName.join(", ")} 셀럽님과의 거래를 취소하시겠습니까?
+                </div>
+                <label>
+                  네
+                  <input
+                    type="radio"
+                    name="cancel"
+                    value="yes"
+                    onChange={(e) => handleCancelStatus(e.target.value)}
+                  />
+                </label>
+                <label>
+                  아니요
+                  <input
+                    type="radio"
+                    name="cancel"
+                    value="no"
+                    onChange={(e) => handleCancelStatus(e.target.value)}
+                  />
+                </label>
+              </div>
+              <div className="bankPage_cancel_inner">
+                <div className="cancel_inner_title">취소 사유</div>
+                <label>
+                  셀럽의 노쇼
+                  <input type="checkbox" value="노쇼" />
+                </label>
+                <label>
+                  일정 문제
+                  <input type="checkbox" value="일정 문제" />
+                </label>
+                <label>
+                  예산 문제
+                  <input type="checkbox" value="예산 문제" />
+                </label>
+                <label>
+                  기타
+                  <input type="checkbox" value="기타" />
+                </label>
+              </div>
+              <button onClick={handleCancelBtn}>제출하기</button>
             </div>
           </>
         )}
